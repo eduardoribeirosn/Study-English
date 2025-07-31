bd542ad83c325f57fe462d735d9dc8ba93f15dbb
Eduardo Nascimento <eduardo.san.nascimento@gmail.com>
2025-07-30 22:17:21 -0300

    Day 1 of Study
    Increasing vocabulary

---
+++ Basico - Iniciante/tutorial.mk
@@ -0,0 +1,103 @@
+# salve 
+
+abc
+**abc**
+__abc__
+*abc*
+_abc_
+**_a b c_**
+~~abc~~
+
+---
+
+***
+
+___
+
+> bla bla bla
+
+> bla 1
+>
+> bla 2
+>
+> bla 3
+
+> bla 1.0
+>
+>> bla 1.1
+
+> #### título
+>
+> - pontinho
+> - ié
+>
+> *sla* isso ae **uhum**.
+
+1. Primeiro
+2. Segundo
+3. Terceiro
+4. Quarto
+
+---
+
+1. Primeiro Item
+2. Segundo Item
+3. Terceiro Item
+    1. Terceiro 2 Item
+    2. Terceiro 3 Item
+4. Quarto Item
+
+- Exemplo 1
+
+* Exemplo 2
+
++ Exemplo 3
+
+- [ ] Esta é uma checkbox não marcada
+
+- [x] Esta é uma checkbox já marcada
+
+` código dentro `
+` let botao = document.getElementById("buttonStore") `
+
+```
+
+Este é nosso
+Querido Exemplo
+**Teste**
+Note que o trecho não ficará em negrito!
+
+```
+
+ ~~~python
+    s = "Sintaxe do Python"
+    print s
+~~~
+
+Clique [aqui](https://github.com) para acessar à página do GitHub.
+
+![Alt ou título da imagem](URL da imagem)
+
+Clique na imagem para acessar o Google.
+[![Logo_Google_2013_Official svg](https://user-images.githubusercontent.com/5532...)](https://google.com.br)
+
+Teste   | Exemplo
+------- | ------
+Teste 1 | Exemplo 1
+Teste 2 | Exemplo 2
+Teste 3 | Exemplo 3
+Teste 4 | Exemplo 4
+
+Alinhado a esquerda | Centralizado | Alinhado a direita
+:--------- | :------: | -------:
+Exemplo | Exemplo | Exemplo
+
+<dl>
+  <dt>Lista definitiva</dt>
+  <dd>É algo que as pessoas usam às vezes.</dd>
+
+  <dt>Markdown no HTML</dt>
+  <dd>*Não* funciona **muito bem**. Use as <em>tags</em> do HTML</dd>
+</dl>
+
+:smile: :fire: :file_folder: :space_invader: :computer:
---
+++ Basico - Iniciante/vocabulario.mk
@@ -0,0 +1,122 @@
+# Vocabulary!!!
+
+## Colors
+
+**Red** = _Vermelho_
+**Green** = _Verde_
+**Blue** = _Azul_
+**Pink** = _Rosa_
+**Purple** = _Roxo_
+**Black** = _Preto_
+**White** = _Branco_
+**Yellow** = _Amarelo_
+**Orange** = _Laranja_
+**Gray** = _Cinza_
+**Cyan** = _Ciano_
+**Brown** = _Marrom_
+
+---
+
+## Days of the Week
+
+**Monday** = _Segunda_
+**Tuesday** = _Terça_
+**Wednesday** = _Quarta_
+**Thursday** = _Quinta_
+**Friday** = _Sexta_
+**Saturday** = _Sábado_
+**Sunday** = _Domingo_
+
+---
+
+## Numbers
+
+**One** = _Um_
+**Two** = _Dois_
+**Three** = _Três_
+**Four** = _Quatro_
+**Five** = _Cinco_
+**Six** = _Seis_
+**Seven** = _Sete_
+**Eight** = _Oito_
+**Nine** = _Nove_
+**Ten** = _Dez_
+
+**Eleven** = _Onze_
+**Twelve** = _Doze_
+**Thirteen** = _Treze_
+**Fourteen** = _Catorze or Quatorze_
+**Fifteen** = _Quinze_
+**Sixteen** = _Dezesseis_
+**Seventeen** = _Dezesete_
+**Eighteen** = _Dezoito_
+**Nineteen** = _Dezenove_
+
+**Twenty** = _Vinte_
+**Thirty** = _Trinta_
+**Forty** = _Quarenta_
+**Fifty** = _Cinquenta_
+**Sixty** = _Sessenta_
+**Seventy** = _Setenta_
+**Eighty** = _Oitenta_
+**Ninety** = _Noventa_
+
+**Hundred** = _Cem_
+**Thousand** = _Mil_
+**Milion** = _Milhão_
+
+---
+
+## Animals
+
+**Bee** = _Abelha_
+**Donkey** = _Burro_
+**Sloth** = _Bixo-Preguiça_
+**Butterfly** = _Borboleta_
+**Dog** = _Cachorro_
+**Rabbit** = _Coelho_
+**Horse** = _Cavalo_
+**Owl** = _Coruja_
+**Elephant** = _Elefante_
+**Seal** = _Foca_
+**Falcon** = _Falcão_
+**Ant** = _Formiga_
+**Flamingo** = _Flamingo_
+**Cat** = _Gato_
+**Raccoon** = _Guaxinim_
+**Giraffe** = _Girafa_
+**Rooster** = _Galo_
+**Hippopotamus** = _Hipopótamo_
+**Hyena** = _Hiena_
+**Hamster** = _Hamster_
+**Alligator** = _Jacaré_
+**Wild Boar** = _Javali_
+**Boa Contrictor** = _Jibóia_
+**LadyBug** = _Joaninha_
+**Lion** = _Leão_
+**Wolf** = _Lobo_
+**Monkey** = _Macaco_
+**Bat** = _Morcego_
+**Sheep** = _Ovelha_
+**Duck** = _Pato_
+**Pig** = _Porco_
+**Fox** = _Raposa_
+**Reindeer** = _Rena_
+**Frog** = _Sapo_
+**Snake** = _Serpente_
+**Turtle** = _Tartaruga_
+**Tiger** = _Tigre_
+**Bear** = _Urso_
+**Cow** = _Vaca_
+**Yeti** = _Yeti_
+**Zebra** = _Zebra_
+
+---
+
+## Random Words
+
+**well** = _bem_
+**how** = _como_
+**long** = _longo_
+**how long** = _quanto tempo_
+**been** = _estado_
